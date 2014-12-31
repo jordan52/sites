15f379b164c61889935468d82fe0f11a2ececc48
jordan <jordan52@gmail.com>
2014-12-31 14:50:28 -0600

    flash now works. added jquery and bootstrap js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  req.flash('success', { msg: 'Flashing message here.'});
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
