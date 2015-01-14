fd81c349d2d2653920c8e20c0683ef8a631a73e6
Jordan Woernde <jordan52@gmail.com>
2015-01-13 20:48:17 -0600

    fixed up mime type for rss feeds

--- app.js
+++ app.js
@@ -60,13 +60,16 @@ app.use(function (req, res, next) {
     }
 });
 app.route('/feed/').get(function(req, res, next) {
-    return res.json(req.app.locals.feed.render('rss-2.0'));
+    res.type('application/rss+xml');
+    return res.send(req.app.locals.feed.render('rss-2.0'));
 });
 app.route('/feed/rss/').get(function(req, res, next) {
-    return res.json(req.app.locals.feed.render('rss-2.0'));
+    res.type('application/rss+xml');
+    return res.send(req.app.locals.feed.render('rss-2.0'));
 });
 app.route('/feed/atom/').get(function(req, res, next) {
-    return res.json(req.app.locals.feed.render('atom-1.0'));
+    res.type('application/rss+xml');
+    return res.send(req.app.locals.feed.render('atom-1.0'));
 });
 app.route('/pages').get(function(req, res, next) {
     return res.json(req.app.pages.getAllPageLinks());
