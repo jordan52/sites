054ab4f943705f018afb198137215da48d112bc8
Jordan Woernde <jordan52@gmail.com>
2015-01-26 22:03:29 -0600

    added sort of a quiz or questionnaire piece to apptest

--- public/js/apptest.js
+++ public/js/apptest.js
@@ -1,6 +1,8 @@
 ;(function() {
     "use strict";
 
+    var question = _.template('<b>name:</b> <%= name %> <b>type:</b> <%= type %> <b>exits:</b> <% _.forEach(exits, function(exit) { %><a href="#/<%- exit %>"><%- exit %></a><% }); %>');
+
     var lower_back = {
         _id:'lower_back',
         type: 'doineedthis',
@@ -18,14 +20,14 @@
             {
                 name:'first_without_radioplathy',
                 type:'router',
-                description: 'select specialist type and initial result ',
+                description: 'firstwithout ',
                 specialist_type:['MD/DO','ORTHO','CHIRO','PAIN'],
                 exits: ['end']
             },
             {
                 name:'first_with_radioplathy',
                 type:'router',
-                description: 'select specialist type and initial result ',
+                description: 'first with ',
                 specialist_type:['MD/DO','ORTHO','CHIRO','PAIN'],
                 exits: ['end']
             },
@@ -51,9 +53,19 @@
 
         return function() {
             deferred.then(function(html) {
-                main.innerHTML = views[which] = html;
+                if(html){
+                    views[which] = html;
+                }
+                main.innerHTML = views[which];
                 decloak();
             });
+        };
+    };
+
+    var loadQuestion = function loadQuestion(n){
+        return function() {
+            main.innerHTML = question(n);
+            decloak();
         };
     };
 
@@ -62,6 +74,12 @@
         r.add('page1', loadView('page1'));
         r.add('page2', loadView('page2'));
         r.add('page3', loadView('page3'));
+
+        _.forEach(lower_back.questions, function(n) {
+            console.log('hello '+ n.name);
+            r.add(n.name, loadQuestion(n));
+        });
+
 
         var update = function update() {
             var hash = location.hash || '#';
