b3d64baabfd0f09fb5232368ead1873fd5fa3402
Jordan Woernde <jordan52@gmail.com>
2015-06-24 15:38:42 -0500

    webcam with fade in

--- public/webcam/main.js
+++ public/webcam/main.js
@@ -10,9 +10,34 @@
 
 // Put variables in global scope to make them available to the browser console.
 var video = document.querySelector('video');
-var canvas = window.canvas = document.querySelector('canvas');
+var canvas = window.canvas = document.querySelector('#layer1');
+var overlay = document.querySelector('#layer2');
+var ga = 0.0;
+var timerId = 0;
 canvas.width = 480;
 canvas.height = 360;
+
+overlay.width = 480;
+overlay.height = 360;
+
+var context = overlay.getContext("2d");
+function fadeIn()
+{
+    context.clearRect(0,0, overlay.width,overlay.height);
+    context.globalAlpha = ga;
+    var ie = new Image();
+    ie.onload = function()
+    {
+        context.drawImage(ie, 0, 0, canvas.width, canvas.height);
+    };
+    ie.src = "brandmeyer.jpg";
+
+    ga = ga + 0.1;
+    if (ga > 1.0)
+    {
+        clearInterval(timerId);
+    }
+}
 
 var button = document.querySelector('button');
 button.onclick = function() {
@@ -20,6 +45,13 @@ button.onclick = function() {
     canvas.height = video.videoHeight;
     canvas.getContext('2d').
         drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    overlay.width = video.videoWidth;
+    overlay.height = video.videoHeight;
+    timerId = setInterval("fadeIn()", 100);
+
+
+
 };
 
 var constraints = {
